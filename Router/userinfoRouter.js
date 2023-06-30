e4e47725618670956c3652dcd4ff6f05cfa3ea9e
Jenny Nguyen <nguyencuc1120@gmail.com>
2023-06-30 15:14:04 -0500

    added userinfo

--- Router/userinfoRouter.js
+++ Router/userinfoRouter.js
@@ -1,1 +1,11 @@
 const Router = require('express').Router()
+const controller = require('../controller/userinfoController')
+
+Router.get('/', controller.finduserinfo) 
+Router.get('/:id', controller.findUserinfoById) 
+Router.get('/brand/:id', controller.findUserinfoId) 
+Router.post('/', controller.createUserinfo) 
+Router.put('/', controller.updateUserinfo) 
+Router.delete('/:id', controller.deleteUserinfo)
+
+module.exports = Router

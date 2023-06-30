72d37a58a18608ff53c3ee6b52f4b2ba81fae7e0
Tsion <tsionwendmu12@gmail.com>
2023-06-30 13:40:55 -0400

    j

--- seed/index.js
+++ seed/index.js
@@ -18,11 +18,87 @@ const main = async () => {
             
           },
           {
-
+            username: "jane_smith",
+            password: "secret456",
+            profileimage: "profile2.jpg",
+            cost: 15.49,
+            isLoggedIn: true,
+            isSeller: true,
+            
           },
           {
-
+            username: "user123",
+            password: "password789",
+            profileimage: "profile3.jpg",
+            cost: 9.99,
+            isLoggedIn: false,
+            isSeller: false, 
           },
-          
+          {
+            username: "testuser",
+            password: "testpass",
+            profileimage: "profile4.jpg",
+            cost: 5.99,
+            isLoggedIn: true,
+            isSeller: true,
+            
+          },
+          {
+            username: "guest",
+            password: "guestpass",
+            profileimage: "profile5.jpg",
+            cost: 12.99,
+            isLoggedIn: false,
+            isSeller: false
+   
+          },
+          {
+            username: "user456",
+            password: "secure789",
+            profileimage: "profile6.jpg",
+            cost: 8.49,
+            isLoggedIn: true,
+            isSeller: true
+            
+          },
+          {
+            username: "user789",
+            password: "password123",
+            profileimage: "profile7.jpg",
+            cost: 6.99,
+            isLoggedIn: false,
+            isSeller: false
+            
+          },
+          {
+            username: "newuser",
+            password: "newpass",
+            profileimage: "profile9.jpg",
+            cost: 3.99,
+            isLoggedIn: false,
+            isSeller: false
+
+            
+          },
+          {
+            username: "user790",
+            password: "password1299",
+            profileimage: "profile8.jpg",
+            cost: 9.99,
+            isLoggedIn: false,
+            isSeller: false
+            
+          },
+          {
+            username: "user999",
+            password: "password999",
+            profileimage: "profile10.jpg",
+            cost: 7.99,
+            isLoggedIn: true
+            
+          }
+
     ]
+
+    await userinfoSchema.de
 }

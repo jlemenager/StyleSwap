93dada5382dc20ec80b874e8ce54544efd61edb2
Tsion <tsionwendmu12@gmail.com>
2023-07-02 11:54:16 -0400

    some

--- front-end/src/components/Home.jsx
+++ front-end/src/components/Home.jsx
@@ -9,31 +9,59 @@ export default function Home() {
         description: ''
     }
     const [formState, setFormState] = useState(initialState)
+
+
     const handleChange = event => {
         setFormState({...formState, [event.target.id]: event.target.value})
     }
     const handleSubmit = event => {
         event.preventDefault()
         console.log(formState)
-        // setFormState(initialState)
-        const postNewPost = async() => {
-            const response = await axios.post(`http://localhost:3001/api/post`, {...formState, username:formState.username, description:formState.description})
-        }
-        postNewPost()  
+        addPost()
+        setFormState(initialState)
+    }
+
+    const addPost = async () => {
+       const res = await axios.post(`http://localhost:3001/api/post`, formState)
+
+       setPosts([...posts, res.data])
     }
 
     return(
-        <div>    
-            <h1>home</h1>
-            {posts.map(post=>(
+        <div>  
+            <div>
+                <form onSubmit={handleSubmit}>
+                    <lable htmlFor='username'>Username</lable>
+                    <input
+                          id='username'
+                          type='text'
+                          onChange={handleChange}
+                          value={formState.username}></input>
+                    <label>Description</label>
+                    <input id='description'
+                           type='text'
+                           onChange={handleChange}
+                           value={formState.description}></input>
+                    <button type='submit'></button>
+                </form>
+            </div>
+
+           <div>
+             {posts.map(post=>(
                 <div key={post.username} className='post'>
                 <h2>{post.username}</h2>
                 <p>{post.description}</p>
                 <p>{post.products}</p>
                 <span>{post.likes}</span>
                 <span>{post.comments}</span>
+                <label>comment</label>
+                <input></input>
+               
                 </div>
-            ))}
+              ))}
+            </div>  
+            <h1>home</h1>
+           
         </div>
     )
 }

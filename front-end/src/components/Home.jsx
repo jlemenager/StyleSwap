a4f675adc9598f36cac02f57169ea0afba7100a5
jlemenager <jeremy@unitedbyzero.com>
2023-07-01 17:30:33 -0700

    adding react connections to be able to post

--- front-end/src/components/Home.jsx
+++ front-end/src/components/Home.jsx
@@ -1,6 +1,19 @@
+import UserContext from "../UserContext"
+import React, { useContext } from 'react'
+
 export default function Home() {
+    const { posts, setPosts } = useContext(UserContext)
     return(
-        <div>
+        <div>    
+            {posts.map(post=>(
+                <div className='post'>
+                <h2>{post.username}</h2>
+                <p>{post.description}</p>
+                <p>{post.products}</p>
+                <span>{post.likes}</span>
+                <span>{post.comments}</span>
+                </div>
+            ))}
         </div>
     )
 }

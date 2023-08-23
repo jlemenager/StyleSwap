48e4457f362f998248cff1fd2129d71453f15042
jlemenager <jeremy@unitedbyzero.com>
2023-08-23 16:39:57 -0400

    added icons

--- front-end/src/components/Nav.jsx
+++ front-end/src/components/Nav.jsx
@@ -4,6 +4,8 @@ import UserContext from '../UserContext'
 import Home from './Home'
 import heroSearch from '../images/heroSearch.png'
 import logo from '../images/logo.png'
+import cart from '../images/cartt.png'
+import product from '../images/product.png'
 
 export default function Nav () {
     const { userFile, setUserFile, handleUserImageUpload } = useContext(UserContext)
@@ -16,11 +18,11 @@ export default function Nav () {
         
             <div className='center-links'>
                  <Link to='/'><img src='src/images/home.png'/></Link>
-                 <Link to='/product'><img className='nav-icon marketplace' src="src/images/product.png"/></Link>
+                 <Link to='/product'><img className='nav-icon marketplace' src={product}/></Link>
             </div>
 
             <div className='left-title'>
-                <Link to='/cart'><img className='nav-icon left cart' src="src/images/cartt.png" alt="cart" /></Link>
+                <Link to='/cart'><img className='nav-icon left cart' src={cart} alt="cart" /></Link>
                  <Link to='/login'><img className='nav-icon left login' src={userFile} alt="user-icon" /></Link> 
             </div>
         </div>
